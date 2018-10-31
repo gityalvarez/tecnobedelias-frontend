c8ebe7c152a7d293b639bc9355e58641be5c08dc
HekutoruMAC <hekutoru.mac@gmail.com>
2018-10-31 04:42:46 -0300

    Diseño: Ingreso y Encabezado

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../_services/auth.service';
 import { TokenStorage } from '../../_helpers/TokenStorage';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { AlertModule } from 'ngx-bootstrap';
 
 @Component({
@@ -11,44 +11,44 @@ import { AlertModule } from 'ngx-bootstrap';
 })
 export class LoginComponent implements OnInit {
 
-  alert = false
+  alert = false;
+  username: string;
+  password: string;
 
-  constructor(private router:Router, private authServ:AuthService,private tokenStorage:TokenStorage) { }
+  constructor(private router: Router, private authServ: AuthService, private tokenStorage: TokenStorage) { }
 
   ngOnInit() {
   }
 
-  username:string;
-  password:string;
+  login(): void {
+    this.authServ.auth(this.username, this.password).subscribe(
+      (data) => {
+        if (data.headers.get('Authorization') != null) {
+          this.tokenStorage.saveToken(data.headers.get('Authorization'));
+          const role = this.tokenStorage.getRole();
+          if (role === 'funcionario') {
 
-  login():void{
-    this.authServ.auth(this.username,this.password).subscribe(
-      (data)=>{
-          if(data.headers.get('Authorization') != null){
-            this.tokenStorage.saveToken(data.headers.get('Authorization'));
-            const role = this.tokenStorage.getRole();
-            if (role == 'funcionario'){
-              
-            this.alert = true
-            this.tokenStorage.signOut()
-            this.password=''
-            this.username=''
-            }else this.router.navigate([role]);
-          }/*else{
+            this.alert = true;
+            this.tokenStorage.signOut();
+            this.password = '';
+            this.username = '';
+          } else { this.router.navigate([role]); }
+        }/*else{
             console.log('usuario no registrado')
             this.password=''
             this.username=''
             this.alert = true
           }*/
-        },
-        (error)=>{
-          this.password=''
-            this.username=''
-            this.alert = true
+      },
+      (error) => {
+        this.password = '';
+        this.username = '';
+        this.alert = true;
 
-        }
+      }
 
-    )}
-    
+    );
   }
+
+}
 
