80769527a5456629c00400515dd0c51507db148a
HekutoruMAC <hekutoru.mac@gmail.com>
2018-10-31 04:40:42 -0300

    Diseño:
    Diseño: Botones y Listas

--- src/app/director/asignar-asignatura/asignar-asignatura.component.ts
+++ src/app/director/asignar-asignatura/asignar-asignatura.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef, HostListener } from '@angular/core';
 import { Carrera } from 'src/app/_models/Carrera';
 import { CarreraService } from 'src/app/_services/carrera.service';
 import { AsignaturaService } from 'src/app/_services/asignatura.service';
 import { Asignatura } from 'src/app/_models/Asignatura';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { Router } from '@angular/router';
-import {DialogModule} from 'primeng/dialog';
 import { Asignatura_Carrera } from 'src/app/_models/Asignatura_Carrera';
-import { Observable } from 'rxjs';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-asignar-asignatura',
@@ -16,91 +15,108 @@ import { Observable } from 'rxjs';
 })
 export class AsignarAsignaturaComponent implements OnInit {
   modalRef: BsModalRef;
-  display: boolean = false;
-  carreras : Carrera[];
-  carrera : Carrera;
-  asignaturas : Asignatura[];
-  asignatura : Asignatura
-  isOpen:boolean;
-  asignaturasFaltantes : Asignatura[];
-  asignaturaCarrera:Asignatura_Carrera;
-  submit:boolean;
+  display = false;
+  carreras: Carrera[];
+  carrera: Carrera;
+  asignaturas: Asignatura[];
+  asignatura: Asignatura;
+  isOpen: boolean;
+  asignaturasFaltantes: Asignatura[];
+  asignaturaCarrera: Asignatura_Carrera;
+  submit: boolean;
 
+  // Pruebas para Deshabilitar tecla Control
+  /*
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvents(e: KeyboardEvent) {
+    if (e.ctrlKey) {
+      e.preventDefault();
+      console.log(e.key);
+      console.log(e.keyCode);
+    }
+  }
 
-  constructor(private router:Router, private carreraService:CarreraService,private asignaturaService:AsignaturaService,private modalService: BsModalService) { 
+  onKey(e) {
+    e.preventDefault();
+    console.log(e.key);
+  }
+  */
+
+  constructor(private router: Router, private carreraService: CarreraService,
+    private asignaturaService: AsignaturaService, private modalService: BsModalService) {
     this.asignaturaCarrera = new Asignatura_Carrera();
   }
 
   ngOnInit() {
     this.carreraService.getCarreras().subscribe(
-      (carreras)=>{
-        this.carreras=carreras;
-      },(error)=>{
+      (carreras) => {
+        this.carreras = carreras;
+      }, (error) => {
         console.log(error);
       },
-    );     
+    );
   }
 
-  cargarListas(carrera:Carrera){
+  cargarListas(carrera: Carrera) {
     this.carrera = carrera;
     this.carreraService.getAsignaturas(carrera).subscribe(
-      (asignaturas)=>{
+      (asignaturas) => {
         this.asignaturas = asignaturas;
       }
     );
-  
+
     this.carreraService.getAsignaturasFaltantes(carrera).subscribe(
-      (asignaturas)=>{
+      (asignaturas) => {
         this.asignaturasFaltantes = asignaturas;
       }
     );
   }
-  
-  agregarAsignatura(carrera,asignatura,asignaturaCarrera): void {
-    console.log(carrera.nombre,asignatura.nombre);
-    //this.items.push(`Item ${this.items.length + 1}`);
-    this.carreraService.asignarAsignatura(carrera,asignatura,asignaturaCarrera).subscribe(
-      (data)=>{
-        if (data){
-          console.log("agregue la asignatura  a la carrera");
-          alert("La asignatura "+asignatura.nombre+" fue asignada correctamente"); 
 
-        }else{
-          alert("La asignatura "+asignatura.nombre+" no pudo ser asignada");
+  agregarAsignatura(carrera, asignatura, asignaturaCarrera): void {
+    console.log(carrera.nombre, asignatura.nombre);
+    // this.items.push(`Item ${this.items.length + 1}`);
+    this.carreraService.asignarAsignatura(carrera, asignatura, asignaturaCarrera).subscribe(
+      (data) => {
+        if (data) {
+          console.log('agregue la asignatura  a la carrera');
+          alert('La asignatura ' + asignatura.nombre + ' fue asignada correctamente');
+
+        } else {
+          alert('La asignatura ' + asignatura.nombre + ' no pudo ser asignada');
         }
       }
     );
   }
 
-  eliminarAsignatura(carrera,asignatura): void {
-    //this.items.push(`Item ${this.items.length + 1}`);
-    this.carreraService.desasignarAsignatura(carrera,asignatura).subscribe(
-      (data)=>{
-        if(data){
-          console.log("elimine la asignatura  a la carrera");
-          alert("La asignatura "+asignatura.nombre+" fue desasignada correctamente"); 
+  eliminarAsignatura(carrera, asignatura): void {
+    // this.items.push(`Item ${this.items.length + 1}`);
+    this.carreraService.desasignarAsignatura(carrera, asignatura).subscribe(
+      (data) => {
+        if (data) {
+          console.log('elimine la asignatura  a la carrera');
+          alert('La asignatura ' + asignatura.nombre + ' fue desasignada correctamente');
 
-        }else{
-          console.log("no se pudo eliminar la asignatura  a la carrera");
-          alert("La asignatura "+asignatura.nombre+" no pude ser desasignada correctamente");
-          this.asignaturasFaltantes.splice(this.asignaturasFaltantes.indexOf(asignatura),1);
+        } else {
+          console.log('no se pudo eliminar la asignatura  a la carrera');
+          alert('La asignatura ' + asignatura.nombre + ' no pude ser desasignada correctamente');
+          this.asignaturasFaltantes.splice(this.asignaturasFaltantes.indexOf(asignatura), 1);
           this.asignaturas.push(asignatura);
         }
       }
     );
   }
 
-  onMoveToSource(event){
-    console.log('entre al onMoveToSource con '+this.carrera.nombre +' y asignatura '+event.items[0]);
-    this.asignatura = event.items[0]
-    this.showDialog() 
-    //this.agregarAsignatura(this.carrera,event.items[0],this.asignaturaCarrera)
+  onMoveToSource(event) {
+    console.log('entre al onMoveToSource con ' + this.carrera.nombre + ' y asignatura ' + event.items[0]);
+    this.asignatura = event.items[0];
+    this.showDialog();
+    // this.agregarAsignatura(this.carrera,event.items[0],this.asignaturaCarrera)
 
   }
 
-  onMoveToTarget(event){
-    console.log('entre al onMoveToTarget con '+this.carrera.nombre +' y asignatura '+event.items[0]);
-    this.eliminarAsignatura(this.carrera,event.items[0])
+  onMoveToTarget(event) {
+    console.log('entre al onMoveToTarget con ' + this.carrera.nombre + ' y asignatura ' + event.items[0]);
+    this.eliminarAsignatura(this.carrera, event.items[0]);
   }
 
 
@@ -108,17 +124,17 @@ export class AsignarAsignaturaComponent implements OnInit {
     this.display = true;
   }
 
-  
-  
 
-  onSubmit(){
-    this.display = false
-    this.agregarAsignatura(this.carrera,this.asignatura,this.asignaturaCarrera)
+
+
+  onSubmit() {
+    this.display = false;
+    this.agregarAsignatura(this.carrera, this.asignatura, this.asignaturaCarrera);
   }
-  
-  cancelDialog(){
-    this.display = false
-    this.asignaturas.splice(this.asignaturasFaltantes.indexOf(this.asignatura),1);
+
+  cancelDialog() {
+    this.display = false;
+    this.asignaturas.splice(this.asignaturasFaltantes.indexOf(this.asignatura), 1);
     this.asignaturasFaltantes.push(this.asignatura);
   }
 
--- src/app/director/asignar-previa/asignar-previa.component.ts
+++ src/app/director/asignar-previa/asignar-previa.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {PickListModule} from 'primeng/picklist';
+import { PickListModule } from 'primeng/picklist';
 import { Carrera } from 'src/app/_models/Carrera';
 import { Asignatura } from 'src/app/_models/Asignatura';
 import { CarreraService } from 'src/app/_services/carrera.service';
@@ -13,100 +13,100 @@ import { Asignatura_Carrera } from 'src/app/_models/Asignatura_Carrera';
 })
 export class AsignarPreviaComponent implements OnInit {
 
-  carreras : Carrera[];
-  carrera : Carrera;
-  asignatura : Asignatura;
-  asignaturas : Asignatura[];
-  previas : Asignatura[];
-  previasPosibles : Asignatura[];
+  carreras: Carrera[];
+  carrera: Carrera;
+  asignatura: Asignatura;
+  asignaturas: Asignatura[];
+  previas: Asignatura[];
+  previasPosibles: Asignatura[];
 
 
-  constructor(private carreraService:CarreraService, private asignaturaService:AsignaturaService) { }
+  constructor(private carreraService: CarreraService, private asignaturaService: AsignaturaService) { }
 
   ngOnInit() {
-    
+
     this.carreraService.getCarreras().subscribe(
-      (carreras)=>{
-        this.carreras=carreras;
-      },(error)=>{
+      (carreras) => {
+        this.carreras = carreras;
+      }, (error) => {
         console.log(error);
       },
-    );    
+    );
   }
 
-  cargarAsignaturas(carrera:Carrera){
+  cargarAsignaturas(carrera: Carrera) {
     this.carrera = carrera;
     this.carreraService.getAsignaturas(carrera).subscribe(
-      (asignaturas)=>{
+      (asignaturas) => {
         this.asignaturas = asignaturas;
-        }
+      }
     );
   }
 
-  cargarPrevias(asignatura:Asignatura){
+  cargarPrevias(asignatura: Asignatura) {
     this.asignatura = asignatura;
-    this.carreraService.getPrevias(asignatura,this.carrera).subscribe(
-      (previas)=>{
-        this.previas=previas;
-      },(error)=>{
+    this.carreraService.getPrevias(asignatura, this.carrera).subscribe(
+      (previas) => {
+        this.previas = previas;
+      }, (error) => {
         console.log(error);
       },
     );
-    this.carreraService.getPreviasPosibles(asignatura,this.carrera).subscribe(
-      (previasPosibles)=>{
-        this.previasPosibles=previasPosibles;
-      },(error)=>{
+    this.carreraService.getPreviasPosibles(asignatura, this.carrera).subscribe(
+      (previasPosibles) => {
+        this.previasPosibles = previasPosibles;
+      }, (error) => {
         console.log(error);
       },
-    );    
+    );
   }
 
-  agregarPrevia(asignaturaPrevia:Asignatura){
-    console.log(this.carrera.nombre,this.asignatura.nombre,asignaturaPrevia.nombre);
-    this.carreraService.asignarPrevia(this.carrera,this.asignatura,asignaturaPrevia).subscribe(
-      (data)=>{
-        if (data){
-          console.log("agregue la asignatura  a la carrera");
-          alert("La asignatura "+asignaturaPrevia.nombre+" fue asignada correctamente"); 
-        }else{
-          console.log("no se pudo agregar la asignatura");
-          alert("La asignatura "+asignaturaPrevia.nombre+" no pudo ser asignada correctamente"); 
-          this.previas.splice(this.previas.indexOf(asignaturaPrevia),1);
-          this.previasPosibles.push(asignaturaPrevia)
+  agregarPrevia(asignaturaPrevia: Asignatura) {
+    console.log(this.carrera.nombre, this.asignatura.nombre, asignaturaPrevia.nombre);
+    this.carreraService.asignarPrevia(this.carrera, this.asignatura, asignaturaPrevia).subscribe(
+      (data) => {
+        if (data) {
+          console.log('agregue la asignatura  a la carrera');
+          alert('La asignatura ' + asignaturaPrevia.nombre + ' fue asignada correctamente');
+        } else {
+          console.log('no se pudo agregar la asignatura');
+          alert('La asignatura ' + asignaturaPrevia.nombre + ' no pudo ser asignada correctamente');
+          this.previas.splice(this.previas.indexOf(asignaturaPrevia), 1);
+          this.previasPosibles.push(asignaturaPrevia);
         }
 
       }
     );
   }
 
-  eliminarPrevia(asignaturaPrevia:Asignatura){
-    console.log(this.carrera.nombre,this.asignatura.nombre,asignaturaPrevia.nombre);
-    this.carreraService.desasignarPrevia(this.carrera,this.asignatura,asignaturaPrevia).subscribe(
-      (data)=>{
-        if (data){
-          console.log("elimine la asignatura  a la carrera");
-          alert("La asignatura "+asignaturaPrevia.nombre+" fue desasignada correctamente"); 
-        }else{
-          console.log("no se pudo agregar la asignatura");
-          alert("La asignatura "+asignaturaPrevia.nombre+" no pudo ser desasignada correctamente"); 
-          this.previasPosibles.splice(this.previasPosibles.indexOf(asignaturaPrevia),1);
-          this.previas.push(asignaturaPrevia)
+  eliminarPrevia(asignaturaPrevia: Asignatura) {
+    console.log(this.carrera.nombre, this.asignatura.nombre, asignaturaPrevia.nombre);
+    this.carreraService.desasignarPrevia(this.carrera, this.asignatura, asignaturaPrevia).subscribe(
+      (data) => {
+        if (data) {
+          console.log('elimine la asignatura  a la carrera');
+          alert('La asignatura ' + asignaturaPrevia.nombre + ' fue desasignada correctamente');
+        } else {
+          console.log('no se pudo agregar la asignatura');
+          alert('La asignatura ' + asignaturaPrevia.nombre + ' no pudo ser desasignada correctamente');
+          this.previasPosibles.splice(this.previasPosibles.indexOf(asignaturaPrevia), 1);
+          this.previas.push(asignaturaPrevia);
         }
       }
     );
   }
 
 
-  onMoveToSource(event){
-    console.log('entre al onMoveToSource con '+this.carrera.nombre +' y asignatura '+event.items[0]);
+  onMoveToSource(event) {
+    console.log('entre al onMoveToSource con ' + this.carrera.nombre + ' y asignatura ' + event.items[0]);
     this.agregarPrevia(event.items[0]);
   }
 
-  onMoveToTarget(event){
-    console.log('entre al onMoveToTarget con '+this.carrera.nombre +' y asignatura '+event.items[0]);
+  onMoveToTarget(event) {
+    console.log('entre al onMoveToTarget con ' + this.carrera.nombre + ' y asignatura ' + event.items[0]);
     this.eliminarPrevia(event.items[0]);
   }
 
-  
+
 
 }
